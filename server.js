2ad9497bde7d713ecc9dbe9394979d68485de2e8
Tasneem Khalid <tasneemakhalid@gmail.com>
2026-09-04 18:58:06 +0300

    add appi vercel

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:3000', 
-        'https://linkedin-front.vercel.app'  
+        'https://linkedin-front.vercel.app',
+        'https://linkedin-front-www.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
